efd95bf500c14c6b575c6b4299b1953810abd797
Taylor <66925922+poxeNe@users.noreply.github.com>
2022-04-17 19:43:00 -0700

    v0.0.11
    started building engine to handle some more stuff and take it out of main.
    
    first npc on screen.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import tcod
 
-from actions import EscapeAction, MovementAction
+from engine import Engine
+from entity import Entity
 from input_handlers import EventHandler
 
 def main() -> None:
@@ -9,8 +10,8 @@ def main() -> None:
     screen_width = 80
     screen_height = 50
     
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
+    # player_x = int(screen_width / 2)
+    # player_y = int(screen_height / 2)
 
     tileset = tcod.tileset.load_tilesheet(
 
@@ -19,6 +20,12 @@ def main() -> None:
     )
 
     event_handler = EventHandler()
+
+    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
+    entities = {npc, player}
+
+    engine = Engine(entities=entities, event_handler=event_handler, player=player)
 
     with tcod.context.new_terminal(
 
@@ -34,26 +41,33 @@ def main() -> None:
 
         while True:
 
-            root_console.print(x=player_x, y=player_y, string="@")
+            # root_console.print(x=player_x, y=player_y, string="@")
+            # root_console.print(x=player.x, y=player.y, string=player.char, fg=player.color)
+            engine.render(console=root_console, context=context)
 
-            context.present(root_console)
+            # context.present(root_console)
+            events = tcod.event.wait()
+            
+            engine.handle_events(events)
+            # root_console.clear()
 
-            for event in tcod.event.wait():
+            # for event in tcod.event.wait():
 
-                # if event.type == "QUIT":
+            #     # if event.type == "QUIT":
 
-                #     raise SystemExit()
-                action = event_handler.dispatch(event)
+            #     #     raise SystemExit()
+            #     action = event_handler.dispatch(event)
 
-                if action is None:
-                    continue
+            #     if action is None:
+            #         continue
                 
-                if isinstance(action, MovementAction):
-                    player_x += action.dx
-                    player_y += action.dy
+            #     if isinstance(action, MovementAction):
+            #         # player_x += action.dx
+            #         # player_y += action.dy
+            #         player.move(dx=action.dx, dy=action.dy)
 
-                elif isinstance(action, EscapeAction):
-                    raise SystemExit()
+            #     elif isinstance(action, EscapeAction):
+            #         raise SystemExit()
 
 
 if __name__ == "__main__":
