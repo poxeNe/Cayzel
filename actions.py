1368e5518421b758570e2ddd1ff1bff2cc881269
Taylor <66925922+poxeNe@users.noreply.github.com>
2022-04-29 15:48:08 -0700

    v0.0.51
    added inventory system and dropping things out of inventory

--- actions.py
+++ actions.py
@@ -103,6 +103,12 @@ class EscapeAction(Action):
 
        raise SystemExit()
 
+class DropItem(ItemAction):
+
+    def perform(self) -> None:
+
+        self.entity.inventory.drop(self.item)
+
 class WaitAction(Action):
 
     def perform(self) -> None:
